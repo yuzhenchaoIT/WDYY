5d790cc8ce3fb90ae74c2522a8d231c667a302e6
yuzhenchaoIT <45099361+yuzhenchaoIT@users.noreply.github.com>
2019-02-13 16:35:33 +0800

    登录界面销毁

--- app/src/main/java/com/baidu/wdyy/HomeActivity.java
+++ app/src/main/java/com/baidu/wdyy/HomeActivity.java
@@ -145,6 +145,7 @@ public class HomeActivity extends AppCompatActivity {
                 req.scope = "snsapi_userinfo";
                 req.state = "wechat_sdk_demo_test";
                 mWxApi.sendReq(req);
+                finish();
                 break;
         }
     }
